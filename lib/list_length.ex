4823a0bbe964138d783a31834f5ce376be680ad6
erik-ozzetti-martins <erik.ozzetti.martins@gamil.com>
2022-05-20 02:33:07 -0300

    quantidade de elementos

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: percorre(list,0)
+
+  defp percorre([], per) do
+    per
   end
+
+  defp percorre([_head | tail], per) do
+    per = per + 1
+    percorre(tail, per)
+  end
+
 end
